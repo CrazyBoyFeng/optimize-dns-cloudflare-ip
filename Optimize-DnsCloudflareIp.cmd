deaa77771e2c5e9ec1fd6519877be4c7c3a1fe8a
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-02-27 22:14:44 +0800

    参数忽略大小写

--- Optimize-DnsCloudflareIp.cmd
+++ Optimize-DnsCloudflareIp.cmd
@@ -6,7 +6,7 @@ SetLocal EnableDelayedExpansion
 Title %~n0
 CD /D "%~dp0"
 
-If "%1"=="Start" (
+If /I "%1"=="Start" (
     Start /Min Cmd /C "%~dpnx0 NoPause"
     Exit
 )
@@ -15,8 +15,8 @@ Set Path=%Path%;%SystemRoot%\system32\WindowsPowerShell\v1.0\
 PowerShell -ExecutionPolicy Unrestricted -NoProfile -File %~dpn0.ps1
 Set ExitCode=%ErrorLevel%
 Echo.
-If "%ExitCode%"=="0" (
-    If Not "%1"=="NoPause" (
+If /I "%ExitCode%"=="0" (
+    If /I Not "%1"=="NoPause" (
         Pause
     )
 ) Else (

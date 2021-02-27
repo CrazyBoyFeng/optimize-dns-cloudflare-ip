07498cdde64dd4096060e6ad7d5b0f84a1112136
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-02-28 00:15:53 +0800

    隐藏注释

--- Optimize-DnsCloudflareIp.cmd
+++ Optimize-DnsCloudflareIp.cmd
@@ -1,10 +1,9 @@
+@Echo Off
 Rem PS1 脚本启动器
 Rem PS1 文件不能直接运行，所以需要这个启动器。
-
-@Echo Off
-SetLocal EnableDelayedExpansion
 Title %~n0
 CD /D "%~dp0"
+SetLocal EnableDelayedExpansion
 
 If /I "%1"=="Start" (
     Start /Min Cmd /C "%~dpnx0 NoPause"
@@ -13,15 +12,15 @@ If /I "%1"=="Start" (
 
 Set Path=%Path%;%SystemRoot%\system32\WindowsPowerShell\v1.0\
 PowerShell -ExecutionPolicy Unrestricted -NoProfile -File %~dpn0.ps1
-Set ExitCode=%ErrorLevel%
+Set ExitCode=!ErrorLevel!
 Echo.
-If /I "%ExitCode%"=="0" (
+If /I "!ExitCode!"=="0" (
     If /I Not "%1"=="NoPause" (
         Pause
     )
 ) Else (
-    Echo Exit Code: %ExitCode%
+    Echo Exit Code: !ExitCode!
     Echo.
     Pause
 )
-Exit %ExitCode%
+Exit !ExitCode!

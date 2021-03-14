fadccc0877beb6ff3135bbfbeda89525e3123e30
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-14 23:53:31 +0800

    简化参数

--- Optimize-DnsCloudflareIP.cmd
+++ Optimize-DnsCloudflareIP.cmd
@@ -10,16 +10,26 @@ CD /D "%~dp0"
 Set Path=!Path!;!SystemRoot!\System32\WindowsPowerShell\v1.0\
 If /I "%1"=="Minimized" (
     Shift /1
-    PowerShell -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Minimized -File %~dpn0.ps1 -ExitEnd %*
+    PowerShell -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Minimized -File %~dpn0.ps1 %*
+    Set ExitCode=!ErrorLevel!
+    If Not "!ExitCode!"=="0" (
+        Echo.
+        Echo Exit Code: !ExitCode! 1>&2
+        Echo.
+        Pause
+    )
 ) Else If /I "%1"=="Hidden" (
     Shift /1
-    PowerShell -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File %~dpn0.ps1 -ExitEnd -ExitError %*
+    PowerShell -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File %~dpn0.ps1 %*
+    Set ExitCode=!ErrorLevel!
+    If Not "!ExitCode!"=="0" (
+        Echo.
+        Echo Exit Code: !ExitCode! 1>&2
+    )
 ) Else (
     PowerShell -ExecutionPolicy Unrestricted -NoProfile -File %~dpn0.ps1 %*
-)
-Set ExitCode=!ErrorLevel!
-If Not "!ExitCode!"=="0" (
+    Set ExitCode=!ErrorLevel!
     Echo.
-    Echo Exit Code: !ExitCode!
+    Pause
 )
 Exit !ExitCode!

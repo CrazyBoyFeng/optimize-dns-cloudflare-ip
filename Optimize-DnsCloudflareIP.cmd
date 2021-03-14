d8a06d9b3070c59361102c7355b0ad458aca47c8
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-14 23:30:42 +0800

    格式化

--- Optimize-DnsCloudflareIP.cmd
+++ Optimize-DnsCloudflareIP.cmd
@@ -4,10 +4,10 @@ Rem PS1 文件不能直接运行所以需要启动器
 Rem 使用参数 Minimized 最小化模式启动
 Rem 使用参数 Hidden 隐藏模式启动
 Rem 其它参数会传递给 PS1 脚本
+SetLocal EnableDelayedExpansion
 Title %~n0
 CD /D "%~dp0"
-SetLocal EnableDelayedExpansion
-Set Path=%Path%;%SystemRoot%\system32\WindowsPowerShell\v1.0\
+Set Path=!Path!;!SystemRoot!\System32\WindowsPowerShell\v1.0\
 If /I "%1"=="Minimized" (
     Shift /1
     PowerShell -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Minimized -File %~dpn0.ps1 -ExitEnd %*
@@ -18,7 +18,7 @@ If /I "%1"=="Minimized" (
     PowerShell -ExecutionPolicy Unrestricted -NoProfile -File %~dpn0.ps1 %*
 )
 Set ExitCode=!ErrorLevel!
-If Not /I "!ExitCode!"=="0" (
+If Not "!ExitCode!"=="0" (
     Echo.
     Echo Exit Code: !ExitCode!
 )
